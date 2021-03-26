50e6236a4417ef9fc6f0026441fe008ecd044c5c
Alpha Vylly <65187737+AlphaLawless@users.noreply.github.com>
2021-03-26 11:54:48 -0300

    create the calculator

---
+++ src/js/calculadora.js
@@ -0,0 +1,5 @@
+let primeiroValor = parseInt(prompt('Digite o primeiro valor: '))
+let segundoValor = parseInt(prompt('Digite o segundo valor: '))
+let resultado = primeiroValor * segundoValor
+
+document.write(`<h2>${primeiroValor} x ${segundoValor} = ${resultado}</h2>`)
