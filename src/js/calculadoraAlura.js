82389c0c2eddd054b18f581db76e38a832c3aa7c
Alpha Vylly <65187737+AlphaLawless@users.noreply.github.com>
2021-03-26 12:27:19 -0300

    Add the new calculator

--- src/js/calculadoraAlura.js
+++ src/js/calculadoraAlura.js
@@ -1,5 +1,7 @@
 let primeiroValor = parseInt(prompt('Digite o primeiro valor: '))
 let segundoValor = parseInt(prompt('Digite o segundo valor: '))
+//Fazendo o menu para escolhemos a operação que queremos na calculadora
 let resultado = primeiroValor * segundoValor
 
+//Imprime o resultado no campo da calculadora
 document.write(`<h2>${primeiroValor} x ${segundoValor} = ${resultado}</h2>`)
