e003098542ff21823ff721909ea93af09bfb4732
Alpha Vylly <65187737+AlphaLawless@users.noreply.github.com>
2021-03-27 22:02:07 -0300

    Update calculadoraAlura.js

--- src/js/calculadoraAlura.js
+++ src/js/calculadoraAlura.js
@@ -2,7 +2,7 @@ let primeiroValor = parseInt(prompt('Digite o primeiro valor: '))
 let segundoValor = parseInt(prompt('Digite o segundo valor: '))
 //Fazendo o menu para escolhemos a operação que queremos na calculadora
 
-var operacao = prompt('Digite 1 para Soma, 2 para subtração, 3 para multiplicação e 4 para divisão')
+let operacao = prompt('Digite 1 para Soma, 2 para subtração, 3 para multiplicação e 4 para divisão')
 
 //Valores das operações que vão ser feitas
 if (operacao == 1){
